044f1f302a7c537c2ea4e125259fe60882f263fe
CJBiohacker <cj.moyses@gmail.com>
2021-10-11 22:23:06 -0300

    "Interfaces e Types"

---
+++ Ling-Ext-Frame-CLI/TypeScipt/index.ts
@@ -0,0 +1,108 @@
+// CONCEITOS BÁSICOS DE TYPESCRIPT
+alert('Pressione F12 e verifique a lista do Console do navegador.')
+console.log('Introdução ao TypeScript');
+
+function soma(a, b: number) {                           // Tipagem de variável/constante. Característica específica do TypeScript.
+    let sum = (a + b);
+    return console.log(`Soma ${a} + ${b} = ${sum}`);
+}
+
+soma(2, 12); // Parâmetros válidos para a função. Sendo 2 números, conforme o tipado na declaração da função.
+
+// soma('a', 'b'); // ERRO ESPERADO: "O argumento do tipo 'string' não é atribuível ao parâmetro do tipo 'number'."
+
+
+// INTERFACES
+// A declaração de interface é outra forma de nomear um tipo de objeto. O TypeScript somente se "preocupa" com a estrutura do valor passado, ou seja, se tem/recebe as propriedades esperadas (que foram declaradas).
+// A interface é semelhante ao 'Type Alias', porém somente a Interface pode ser modificada após criada. E o 'Type Alias', possui método de declaração via atribuição.
+
+// Criação da interface e declaração de cada atributo/característica. ( '|' = OR  '&' = AND )
+interface Monstro {
+    nome: string
+    tipo: 'Beasts' | 'Cursed One' | 'Draconids' | 'Elementa' | 'Hybrid' | 'Insectoid' | 'Necrophage' | 'Ogroid' | 'Relict' | 'Specter' | 'Vampire'
+    status: boolean     // true = vivo , false = morto
+}
+
+// Interface 'Hybrid' que é estendida da geral 'Monstro'
+interface Hybrid extends Monstro {
+    inteligencia: 'Senciente' | 'Instintivo';
+    tipo: 'Hybrid'
+}
+
+// Atribuição da interface 'Monstro' à uma variável/constante.    
+const succubus: Monstro = {
+    nome: 'Succubus',
+    tipo: "Hybrid",
+    status: true
+}
+
+console.log(`Nome: ${succubus.nome} - Tipo: ${succubus.tipo}`);
+
+// Adicionando uma função/método dentro da interface estendida 'Hybrid'.
+interface Hybrid extends Monstro {
+    seduzirOponente(action: boolean): void;
+    restaurarMana(pontosMana: number): void;
+}
+
+// Atribuição da interface estendida 'Hybrid' à uma variável/constante. (Incluindo a característica/atributo 'inteligencia')
+const succubi: Hybrid = {
+    nome: 'Salma the Succubus',
+    tipo: 'Hybrid',
+    status: true,
+    inteligencia: 'Senciente',
+    seduzirOponente(action) {           // Função clássica do JS.
+        return action;
+    },
+    restaurarMana: (pontosMana) => (`${pontosMana} pontos`)         // Arrow Function ES6+.
+}
+
+console.log(`Nome: ${succubi.nome} 
+Tipo: ${succubi.tipo}
+Inteligência: ${succubi.inteligencia}
+Status: ${succubi.status}
+Seduzir: ${succubi.seduzirOponente(true)}
+Restaurar Mana: ${succubi.restaurarMana(15)}`);
+
+
+// TYPE ALIAS
+// Conforme dito acima, o 'type' funciona de forma parecida com a interface, mas com a limitação de não sofrer alteração nos seus atributos/características.
+
+// Declaração de um type.
+type Pessoa = {
+    nome: string;
+    cor: string;
+    altura: number;
+    peso: number;
+}
+
+// Exemplo de aplicação de type. (Relação entre interfaces)
+interface Humano {
+    nome: string;
+    cor: string;
+    altura: number;
+    peso: number;
+}
+
+interface HomoSapiens extends Humano {
+    inteligência: 'Baixa' | 'Média' | 'Avançada'
+    postura: 'Ereta'
+}
+
+interface Sahelanthropus extends Humano {
+    mobilidade: 'Quadrúpede' | 'Bípede'
+}
+
+// Type de relação entre as 2 interfaces.
+type SubFamiliaHumano = HomoSapiens | Sahelanthropus;
+
+const humano: SubFamiliaHumano = {
+    nome: 'Michael J Vincent',
+    cor: 'Branco',
+    altura: 1.72,
+    peso: 79,
+    mobilidade: 'Bípede',
+    inteligência: "Média",
+    postura: "Ereta"
+}
+
+console.log();
