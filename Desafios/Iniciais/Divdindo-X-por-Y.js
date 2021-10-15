bfb22cf4ca82ca3d8275089f100a37f367e10f7f
CJBiohacker <cj.moyses@gmail.com>
2021-10-14 22:11:19 -0300

    "Desafio Inicial  1/3"

--- Desafios/Iniciais/Divdindo-X-por-Y.js
+++ Desafios/Iniciais/Divdindo-X-por-Y.js
@@ -10,3 +10,24 @@ Saída
 Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo.
 */
 
+let x = Math.round(Math.random() * 100);
+let y = Math.round(Math.random() * 100);
+let N = 100;
+
+console.log("Digite quantas divisões serão realizadas:", N);
+
+for (let i = 1; i <= N; i++) {
+
+    console.log("Digite o valor de x. => x = ", x);
+    console.log("Digite o valor de y. => y = ", y);
+
+    if (x == 0) {
+        console.log("Divisão impossível !!!");
+    } else {
+        console.log(x / y);
+
+    }
+    x = Math.round(Math.random() * 100);
+    y = Math.round(Math.random() * 100);
+
+};
