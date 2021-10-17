23c806e4441dedea1fcf22e3229fba1a052cbd2b
CJBiohacker <cj.moyses@gmail.com>
2021-10-17 17:44:52 -0300

    "Script de interação do jogo"

---
+++ Projetos/Jogo-da-Memoria(Genius)/script.js
@@ -0,0 +1,7 @@
+// Relação entre número e cor.
+// 1 = verde , 2 = vermelho , 3 = azul , 4 = amarelo
+
+let ordem = [];                 // Array com a sequência de desafio do jogo.
+let ordemClicada = [];          // Array com a sequência jogada pelo usuário.
+let pontuacao;                  // Contagem de pontos do usuário
+
